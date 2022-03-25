057cf79c172ce5a951d71e17cfdd1d1af30454d1
Yusuf Çalışır <99292717+stratocasting@users.noreply.github.com>
2022-03-25 14:19:56 +0300

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,15 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+# new_q = Question("fsaf", "False")
+# print(new_q.answer)
+
+# buradaki q_text tamamen anlamsız x de yapılabilir vs vs. self zaten kendisi ve istenilen tek şey
+# oraya .text yazdığımız zaman square brackets 0 gibi çalışmasını istememiz (  [0]   ) aşağıdaki örnek daha
+# açıklayıcı olacak
+
+
